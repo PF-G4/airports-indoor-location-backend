388fc08340a088216016c78e856fdbeba268c2f9
JnsCas <jnss.cas@gmail.com>
2018-04-21 20:17:10 -0300

    initial commit

---
+++ src/main/java/afinal/proyecto/cuatro/grupo/Application.java
@@ -0,0 +1,12 @@
+package afinal.proyecto.cuatro.grupo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
