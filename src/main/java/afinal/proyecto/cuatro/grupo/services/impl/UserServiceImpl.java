749c064d348d9367304b7a443d243238362bd64e
JnsCas <jnss.cas@gmail.com>
2018-05-05 19:23:35 -0300

    elimino validaciones redundantes

--- src/main/java/afinal/proyecto/cuatro/grupo/services/impl/UserServiceImpl.java
+++ src/main/java/afinal/proyecto/cuatro/grupo/services/impl/UserServiceImpl.java
@@ -1,16 +1,11 @@
 package afinal.proyecto.cuatro.grupo.services.impl;
 
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import afinal.proyecto.cuatro.grupo.dao.DaoUser;
 import afinal.proyecto.cuatro.grupo.entities.User;
 import afinal.proyecto.cuatro.grupo.exceptions.UserDuplicateEmailException;
-import afinal.proyecto.cuatro.grupo.exceptions.UserInvalidEmailException;
 import afinal.proyecto.cuatro.grupo.exceptions.UserNotFoundException;
 import afinal.proyecto.cuatro.grupo.services.UserService;
 import afinal.proyecto.cuatro.grupo.services.util.ServiceUtil;
@@ -23,9 +18,7 @@ public class UserServiceImpl extends ServiceUtil implements UserService {
 	
 	@Override
 	public void saveOrUpdate(User user) {
-		if (!isValidEmail(user.getEmail())) throw new UserInvalidEmailException(user.getEmail());
 		try {
-			System.out.println("Entro");
 			daoUser.save(user);
 		} catch (Exception e) {
 			if (isConstraintName("unique_email", e)) throw new UserDuplicateEmailException(user.getEmail());
