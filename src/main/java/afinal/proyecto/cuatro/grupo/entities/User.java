e478987042182f4ba57af7326fb6c8d6e26b2194
JnsCas <jnss.cas@gmail.com>
2018-05-05 12:38:50 -0300

    agrego campo password a tabla User

--- src/main/java/afinal/proyecto/cuatro/grupo/entities/User.java
+++ src/main/java/afinal/proyecto/cuatro/grupo/entities/User.java
@@ -24,13 +24,17 @@ public class User {
 	@Column(unique = true)
 	private String email;
 	
+	@NotNull
+	private String password;
+	
 	public User() {
 		
 	}
 	
-	public User(String name, String email) {
+	public User(String name, String email, String password) {
 		this.name = name;
 		this.email = email;
+		this.password = password;
 	}
 
 	public Long getId() {
@@ -50,6 +54,14 @@ public class User {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 
--- src/main/java/afinal/proyecto/cuatro/grupo/services/impl/UserServiceImpl.java
+++ src/main/java/afinal/proyecto/cuatro/grupo/services/impl/UserServiceImpl.java
@@ -1,5 +1,9 @@
 package afinal.proyecto.cuatro.grupo.services.impl;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +25,7 @@ public class UserServiceImpl extends ServiceUtil implements UserService {
 	public void saveOrUpdate(User user) {
 		if (!isValidEmail(user.getEmail())) throw new UserInvalidEmailException(user.getEmail());
 		try {
+			System.out.println("Entro");
 			daoUser.save(user);
 		} catch (Exception e) {
 			if (isConstraintName("unique_email", e)) throw new UserDuplicateEmailException(user.getEmail());
