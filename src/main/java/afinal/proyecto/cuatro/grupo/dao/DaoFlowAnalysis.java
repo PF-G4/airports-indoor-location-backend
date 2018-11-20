abf2ca268e3334fef3b215623b0a85a218501222
JnsCas <jnss.cas@gmail.com>
2018-11-20 15:34:34 -0300

    flowAnalysis

--- src/main/java/afinal/proyecto/cuatro/grupo/dao/DaoFlowAnalysis.java
+++ src/main/java/afinal/proyecto/cuatro/grupo/dao/DaoFlowAnalysis.java
@@ -1,7 +1,18 @@
 package afinal.proyecto.cuatro.grupo.dao;
 
 import afinal.proyecto.cuatro.grupo.entities.FlowAnalysis;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
 
 public interface DaoFlowAnalysis extends CrudRepository<FlowAnalysis, Long> {
+
+    @Query("select distinct count(f.idUsuario) from FlowAnalysis f where f.zone = :zone and f.momentoPosicion >= :since and f.momentoPosicion < :until")
+    int peopleQuantity(@Param("zone") String zone, @Param("since") String since, @Param("until") String until);
+
+    @Query("select distinct count(f.idUsuario) from FlowAnalysis f where f.zone = :zone and f.momentoPosicion >= :since")
+    int peopleQuantity(@Param("zone") String zone, @Param("since") String since);
+
 }
