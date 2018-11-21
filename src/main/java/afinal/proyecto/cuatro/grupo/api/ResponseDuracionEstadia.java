14e824df0125795f51f38861201eb2dffebe2c61
JnsCas <jnss.cas@gmail.com>
2018-11-20 21:23:11 -0300

    flow analysis consulta la cantidad de gente en una determinada zona

---
+++ src/main/java/afinal/proyecto/cuatro/grupo/api/ResponseDuracionEstadia.java
@@ -0,0 +1,23 @@
+package afinal.proyecto.cuatro.grupo.api;
+
+public class ResponseDuracionEstadia {
+
+    private Double duracionEstadia;
+
+    public ResponseDuracionEstadia(Double duracionEstadia) {
+        this.duracionEstadia = duracionEstadia;
+    }
+
+    public ResponseDuracionEstadia() {
+
+    }
+
+    public Double getDuracionEstadia() {
+        return duracionEstadia;
+    }
+
+    public void setDuracionEstadia(Double duracionEstadia) {
+        this.duracionEstadia = duracionEstadia;
+    }
+
+}
--- src/main/java/afinal/proyecto/cuatro/grupo/controllers/FlowAnalysisController.java
+++ src/main/java/afinal/proyecto/cuatro/grupo/controllers/FlowAnalysisController.java
@@ -1,15 +1,18 @@
 
 package afinal.proyecto.cuatro.grupo.controllers;
 
+import afinal.proyecto.cuatro.grupo.api.ResponseDuracionEstadia;
 import afinal.proyecto.cuatro.grupo.api.ResponsePeopleQuantity;
 import afinal.proyecto.cuatro.grupo.entities.FlowAnalysis;
 import afinal.proyecto.cuatro.grupo.services.FlowAnalysisService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
 
 @RestController
 @RequestMapping("/flowAnalysis")
@@ -17,6 +20,9 @@ public class FlowAnalysisController {
 
 	@Autowired
 	private FlowAnalysisService flowAnalysisService;
+
+	@PersistenceUnit
+	private EntityManagerFactory entityManagerFactory;
 
 	@RequestMapping(method = RequestMethod.POST)
 	@ResponseStatus(value = HttpStatus.CREATED)
@@ -36,6 +42,22 @@ public class FlowAnalysisController {
 												 @RequestParam(value = "since", defaultValue = "")  String since,
 												 @RequestParam(value = "until", defaultValue = "") String until) {
 		return flowAnalysisService.peopleQuantity(zone, since, until);
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/duracionEstadia")
+	@ResponseStatus(value = HttpStatus.OK)
+	public ResponseDuracionEstadia duracionEstadia(@RequestParam(value = "zone", defaultValue = "") String zone) {
+		String queryStr = "select duracion_estadia from demora_actual where zone = ?1";
+		try {
+			EntityManager entityManager = entityManagerFactory.createEntityManager();
+			Query query = entityManager.createNativeQuery(queryStr);
+			query.setParameter(1, zone);
+
+			return new ResponseDuracionEstadia(Double.parseDouble(query.getSingleResult().toString()));
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
 }
