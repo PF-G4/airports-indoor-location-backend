520e838f29c0de80b0602a3d1f43617507681275
JnsCas <jnss.cas@gmail.com>
2018-11-29 10:53:29 -0300

    agrego excepcion

--- src/main/java/afinal/proyecto/cuatro/grupo/services/impl/TiempoPromedioZonaServiceImpl.java
+++ src/main/java/afinal/proyecto/cuatro/grupo/services/impl/TiempoPromedioZonaServiceImpl.java
@@ -2,6 +2,7 @@ package afinal.proyecto.cuatro.grupo.services.impl;
 
 import afinal.proyecto.cuatro.grupo.dao.DaoTiempoPromedioZona;
 import afinal.proyecto.cuatro.grupo.entities.TiempoPromedioZona;
+import afinal.proyecto.cuatro.grupo.exceptions.TiempoPromedioZonaNotFoundException;
 import afinal.proyecto.cuatro.grupo.services.TiempoPromedioZonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,7 @@ public class TiempoPromedioZonaServiceImpl implements TiempoPromedioZonaService 
     @Override
     public TiempoPromedioZona tiempoPromedioZona(String zone) {
         return daoTiempoPromedioZona.findByZona(zone)
-                //.orElseThrow(() -> new TiempoPromedioZonaNotFoundException("zona", zone));
-                .orElse(new TiempoPromedioZona(zone, 0.0)); //por default, el tiempoProm es 0 FIXME dejar la excepcion
+                .orElseThrow(() -> new TiempoPromedioZonaNotFoundException("zona", zone));
+                //.orElse(new TiempoPromedioZona(zone, 0.0)); //por default, el tiempoProm es 0
     }
 }
